1c02dafe695a1faa2fcf9a5fe1a39099b57aa008
shinate <shine.wangrs@gmail.com>
2016-09-02 21:49:20 +0800

    update

--- js/src/main.js
+++ js/src/main.js
@@ -3,11 +3,11 @@
  */
 
 require(['q/face', 'q/track'], function (face, track) {
-    // face();
-    //
-    // $('.main-icon a').click(function () {
-    //     track();
-    // });
+    face();
 
-    track();
+    $('.main-icon a').click(function () {
+        track();
+    });
+
+    // track();
 });
